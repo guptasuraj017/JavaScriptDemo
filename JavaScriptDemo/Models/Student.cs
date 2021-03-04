6aa08851552ede76cb6e2047b06b9e53659c0156
Suraj Kumar <guptasuraj017@gmail.com>
2021-03-04 23:25:32 +0530

    StudentPropertiesAdded

--- JavaScriptDemo/Models/Student.cs
+++ JavaScriptDemo/Models/Student.cs
@@ -8,6 +8,10 @@ namespace JavaScriptDemo.Models
     public class Student
     {
         public int Id { get; set; }
+        public string Name  { get; set; }
+        public int Age  { get; set; }
+        public string Gender  { get; set; }
+
 
     }
 }
